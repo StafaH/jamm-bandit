f1ea5dfebba5ad157a864bf2951c5b7c7d099c2c
Allen Bao <allenbao06@gmail.com>
2020-11-02 17:52:53 -0500

    regression function

---
+++ logistic_regression.py
@@ -0,0 +1,31 @@
+import numpy as np
+import statsmodels.api as sm
+
+features = 20 # would normally by 512
+trials = 1000 
+coef = np.zeros(features,) # this is the prior
+
+def logistic_reg(coef):
+    
+    def rand_bin_array(weights, N):
+        arr = np.zeros(N)
+        for i in range(0, len(weights)):
+            if weights[i][features-1] > 2:
+                arr[i] = 1
+        return arr
+    
+    weights = np.random.normal(loc=coef[0], scale=1.0, size=(trials,1)) # weights sampled from a normal dist
+    for c in coef[1:]:
+        weight = np.random.normal(loc=c, scale=1.0, size=(trials,1))
+        weights = np.hstack((weights, weight))
+    weights = np.clip(weights, -3, 3)
+    labels = rand_bin_array(weights, trials)
+    
+    result = sm.Logit(labels,weights).fit()
+    coef = result.params
+    p_vals = result.pvalues
+    #print(result.summary())
+    
+    return coef # updates coefficients
+
+coef = logistic_reg(coef)

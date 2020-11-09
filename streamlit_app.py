b8974af429d84f2db64620c33ca9601a0bcdb642
molly <mollysun@icloud.com>
2020-11-09 10:49:48 -0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -63,14 +63,14 @@ def main():
     col2.image(image_out2, use_column_width=True)
     
 
-    if col1.button('Yes'):
+    if col1.button('This one'):
         new_result = {
             'reward': "yes",
             'latents': weights_str
         }
         user.insert_one(new_result)
 
-    if col2.button('No'):
+    if col2.button('This one'):
         new_result = {
             'reward': "no",
             'latents': weights_str

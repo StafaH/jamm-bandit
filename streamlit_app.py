3302a087f01702fa1e5b212d3d372c899f05e9bd
Allen Bao <allenbao06@gmail.com>
2020-11-09 13:47:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,71 +2,46 @@ import numpy as np
 import os
 import urllib
 from datetime import datetime
-
 import torch
 import stylegan2
 from stylegan2 import utils
-
 import streamlit as st
 from streamlit.report_thread import get_report_ctx
 from streamlit.server.server import Server
 from streamlit.hashing import _CodeHasher
-
 from pymongo import MongoClient
-
 from logistic_regression import random_weights
 
 def main():
-
     # session state for persistent values
     state = _get_state()
-
     # Set up database connection
     client = get_database_connection()  
     results = client.results
-
-
     now = datetime.now()
     user = results[get_unique_user_id()]
-
-    #Instructions
-    st.title("Thank you for your interest in our app! Before you get a chance to look at the different faces, you will first be asked to fill out some demographic questions. After answering the demographic question you will then be able to look at different faces. Please select the face that appears to be more aggressive to you by pressing either the X or Y button.")
-
-    # Demographic page
-    st.header('Please fill this out before starting!')
-    st.slider('Age')
-    st.write.number_input('Age', min_value=12, max_value=100)
-    st.selectbox('Gender',('Male', 'Female', 'Other'))
-    st.write.selectbox('Political Orientation', ('Very Liberal', 'Moderately Liberal', 'Slightly Liberal', 'Neither Liberal or Conservative', 'Very Conservative', 'Moderately Conservative', 'Slightly Conservative'))
-    st.write.selectbox('Ethnicity', ('White', 'Hispanic', 'Black', 'Middle Eastern', 'South Asian', 'South-East Asian', 'East Asian', 'Pacific Islander', 'Native American/Indigenous'))
-    st.button('Submit')
-
-
-    #Faces
-    st.header('Which face is more aggressive?')
-
+    st.title('Which face is more aggressive?')
     # Download the model file
     download_file('Gs.pth')
-
     # Load the StyleGAN2 Model
     G = load_model()
     G.eval()
-
-
-
-
+    # Feature Sliders 
+    st.sidebar.title('Please fill this out before starting!')
+    st.sidebar.number_input('Age', min_value=12, max_value=100)
+    st.sidebar.selectbox('Gender', ('Male', 'Female', 'Other'))
+    st.sidebar.selectbox('Ethnicity', ('White', 'Hispanic', 'Black', 'Middle Eastern', 'South Asian', 'South-East Asian', 'East Asian', 'Pacific Islander', 'Native American/Indigenous'))
+    st.sidebar.selectbox('Political Orientation', ('Very Liberal', 'Moderately Liberal', 'Slightly Liberal', 'Neither Liberal or Conservative', 'Very Conservative', 'Moderately Conservative', 'Slightly Conservative'))
+    st.sidebar.button('Submit')
     #default_control_features = ['Male']
-
     # Update the weights
     #rnd = np.random.RandomState(6600)
     #latents = rnd.randn(512)
     weights = random_weights()
     weights_str = np.array_str(weights, precision = 6, suppress_small = True)
-
     # Generate the image
     image_out = generate_image(G, weights)
     image_out2 = generate_image(G, weights)
-
     # Output the image
     col1, col2 = st.beta_columns(2)
     col1.image(image_out, use_column_width=True)
@@ -86,13 +61,11 @@ def main():
             'latents': weights_str
         }
         user.insert_one(new_result)
-
     if st.button('There is something wrong with this picture!'):
         pass
-
     # Mandatory to avoid rollbacks with widgets, must be called at the end of your app
     state.sync()
-
+    
 @st.cache
 def get_unique_user_id():
     now = datetime.now()
@@ -110,16 +83,13 @@ def get_database_connection():
 @st.cache(show_spinner=False)
 def generate_image(G, weights):
     latent_size, label_size = G.latent_size, G.label_size
-
     device = torch.device('cpu')
     if device.index is not None:
         torch.cuda.set_device(device.index)
     G.to(device)
-
     G.set_truncation(0.5)
     noise_reference = G.static_noise()
     noise_tensors = [[] for _ in noise_reference]
-
     latents = []
     labels = []
     rnd = np.random.RandomState(6600)
@@ -127,26 +97,24 @@ def generate_image(G, weights):
             
     for i, ref in enumerate(noise_reference):
         noise_tensors[i].append(torch.from_numpy(rnd.randn(*ref.size()[1:])))
-
     if label_size:
         labels.append(torch.tensor([rnd.randint(0, label_size)]))
-
+    
     latents = torch.stack(latents, dim=0).to(device=device, dtype=torch.float32)
     if labels:
         labels = torch.cat(labels, dim=0).to(device=device, dtype=torch.int64)
     else:
         labels = None
-
+    
     noise_tensors = [torch.stack(noise, dim=0).to(device=device, dtype=torch.float32) for noise in noise_tensors]
-
     if noise_tensors is not None:
         G.static_noise(noise_tensors=noise_tensors)
     with torch.no_grad():
         generated = G(latents, labels=labels)
-
+    
     images = utils.tensor_to_PIL(
         generated, pixel_min=-1, pixel_max=1)
-
+    
     for img in images:
         #img.save('static/images/seed6600.png')
         return img
@@ -154,13 +122,12 @@ def generate_image(G, weights):
 @st.cache(allow_output_mutation=True)
 def load_model():
     return stylegan2.models.load('Gs.pth')
-
+    
 @st.cache(suppress_st_warning=True)
 def download_file(file_path):
     # Don't download the file twice. (If possible, verify the download using the file length.)
     if os.path.exists(file_path):
         return
-
     # These are handles to two visual elements to animate.
     weights_warning, progress_bar = None, None
     try:
@@ -177,19 +144,16 @@ def download_file(file_path):
                         break
                     counter += len(data)
                     output_file.write(data)
-
                     # We perform animation by overwriting the elements.
                     weights_warning.warning("Downloading %s... (%6.2f/%6.2f MB)" %
                         (file_path, counter / MEGABYTES, length / MEGABYTES))
                     progress_bar.progress(min(counter / length, 1.0))
-
     # Finally, we remove these visual elements by calling .empty().
     finally:
         if weights_warning is not None:
             weights_warning.empty()
         if progress_bar is not None:
             progress_bar.empty()
-
 
 class _SessionState:
     def __init__(self, session, hash_funcs):
@@ -201,25 +165,21 @@ class _SessionState:
             "is_rerun": False,
             "session": session,
         }
-    
     def __call__(self, **kwargs):
         """Initialize state data once."""
         for item, value in kwargs.items():
             if item not in self._state["data"]:
                 self._state["data"][item] = value
-    
     def __getitem__(self, item):
         """Return a saved state value, None if item is undefined."""
         return self._state["data"].get(item, None)
-            
+        
     def __getattr__(self, item):
         """Return a saved state value, None if item is undefined."""
         return self._state["data"].get(item, None)
-    
     def __setitem__(self, item, value):
         """Set state value."""
         self._state["data"][item] = value
-    
     def __setattr__(self, item, value):
         """Set state value."""
         self._state["data"][item] = value
@@ -231,7 +191,6 @@ class _SessionState:
     
     def sync(self):
         """Rerun the app with all state values up to date from the beginning to fix rollbacks."""
-    
         # Ensure to rerun only once to avoid infinite loops
         # caused by a constantly changing state value at each run.
         #
@@ -243,27 +202,21 @@ class _SessionState:
             if self._state["hash"] != self._state["hasher"].to_bytes(self._state["data"], None):
                 self._state["is_rerun"] = True
                 self._state["session"].request_rerun()
-    
         self._state["hash"] = self._state["hasher"].to_bytes(self._state["data"], None)
+
+def _get_session():
+    session_id = get_report_ctx().session_id
+    session_info = Server.get_current()._get_session_info(session_id)
+    if session_info is None:
+        raise RuntimeError("Couldn't get your Streamlit Session object.")
     
-    
-    def _get_session():
-        session_id = get_report_ctx().session_id
-        session_info = Server.get_current()._get_session_info(session_id)
-    
-        if session_info is None:
-            raise RuntimeError("Couldn't get your Streamlit Session object.")
-    
-        return session_info.session
-    
-    
-    def _get_state(hash_funcs=None):
-        session = _get_session()
-    
-        if not hasattr(session, "_custom_session_state"):
-            session._custom_session_state = _SessionState(session, hash_funcs)
-    
-        return session._custom_session_state
+    return session_info.session
+
+def _get_state(hash_funcs=None):
+    session = _get_session()
+    if not hasattr(session, "_custom_session_state"):
+        session._custom_session_state = _SessionState(session, hash_funcs)
+    return session._custom_session_state
 
 if __name__ == "__main__":
     main()

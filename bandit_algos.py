d8e3f0d596c570638a120aaa68ecc9b3d1804f15
StafaH <mhaiderbhai@hotmail.com>
2020-11-10 01:00:36 -0500

    Remove excess code

--- bandit_algos.py
+++ bandit_algos.py
@@ -10,7 +10,7 @@ def random_latents():
     weights = np.random.normal(loc=0, scale=1.0, size=(512,))
     return weights
 
-def thompson_sample(observation, prior):
+def bayesian_linear_reg(observation, prior):
     weights = np.random.normal(loc=0, scale=1.0, size=(512,))
     return weights
 
@@ -22,27 +22,15 @@ def limited_thompson_sample():
     weights = np.random.normal(loc=0, scale=1.0, size=(512,))
     return weights
 
-#features = 20 # would normally by 512
-#trials = 1000 
-#coef = np.zeros(features,) # this is the prior
-
 def logistic_reg(weights, observations):
-    
     '''
-    weights = np.random.normal(loc=coef[0], scale=1.0, size=(trials,1)) # weights sampled from a normal dist
-    for c in coef[1:]:
-        weight = np.random.normal(loc=c, scale=1.0, size=(trials,1))
-        weights = np.hstack((weights, weight))
+    THIS WAS A FAILURE. THIS IS NOT GOING TO WORK! :(:(:(
     '''
-    #labels = rand_bin_array(weights, trials)
-    
     result = sm.Logit(observations, weights).fit()
     weights = result.params
     st.text(result.params)
     weights = np.clip(weights, -3, 3)
     st.text(result.summary())
-    #p_vals = result.pvalues
-    #print(result.summary())
     
-    return weights # updates coefficients
+    return weights
     

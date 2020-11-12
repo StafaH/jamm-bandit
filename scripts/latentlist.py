d65bfd838697ce27497012e089c9f90558d7e496
Allen Bao <allenbao06@gmail.com>
2020-11-11 22:28:17 -0500

    latentlist to csv

--- scripts/latentlist.py
+++ scripts/latentlist.py
@@ -1,6 +1,12 @@
 import numpy as np
+import pandas as pd
 latentlist = []
 
 for i in range(6600, 9600):
     np.random.seed(i)
     latentlist.append(np.random.randn(1, 512))
+latentlist = np.asarray(latentlist)
+latentlist = np.reshape(latentlist, [3000, 512])
+    
+df = pd.DataFrame(latentlist)
+df.to_csv('latentlist.csv')

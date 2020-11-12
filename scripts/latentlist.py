a2e8b53ef0952472b6a705dc1806e853313ece2f
Allen Bao <allenbao06@gmail.com>
2020-11-11 22:34:45 -0500

    latentlist seeds

--- scripts/latentlist.py
+++ scripts/latentlist.py
@@ -1,12 +1,17 @@
 import numpy as np
 import pandas as pd
 latentlist = []
+seeds = []
 
 for i in range(6600, 9600):
     np.random.seed(i)
     latentlist.append(np.random.randn(1, 512))
+    seeds.append(i)
+    
 latentlist = np.asarray(latentlist)
 latentlist = np.reshape(latentlist, [3000, 512])
     
 df = pd.DataFrame(latentlist)
+seedseries = pd.Series(seeds)
+df.insert(loc=0, column='seed', value=seedseries)
 df.to_csv('latentlist.csv')

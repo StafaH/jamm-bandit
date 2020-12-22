e6197fa27f8286335638a38e25879f45dcfafaee
Emmy Liu <emmy.liu.1998@gmail.com>
2020-12-22 17:00:30 -0500

    add random sampling code (needs to be injected into template)

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 from .models import arms, users
 
@@ -17,3 +18,10 @@ def db(request):
     arms = arms.objects.all()
 
     return render(request, "db.html", {"arms": arms})
+
+def sample_uniform(request):
+    if request.method == "GET":
+        all_arms = arms.objects.all()
+        random_choice = random.sample(all_arms, 2)
+        return render(request, "index.html", {"choice1": random_choice[0].imgID, "choice2": random_choice[1].imgID})
+    

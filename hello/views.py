8d7b34fafba45be9baa5efd8df05daad995988db
Mustafa H <mhaid@cs.toronto.edu>
2020-12-27 03:13:01 -0500

    Fix bug returning user not seeing any surveys

--- hello/views.py
+++ hello/views.py
@@ -50,7 +50,7 @@ def user_login(request):
             # Save session as cookie to login the user
             login(request, user)
             # Success, now let's login the user.
-            return render(request, "profile.html")
+            return redirect('profile')
         else:
             # Incorrect credentials, let's throw an error to the screen.
             return render(request, "login.html", {'error_message': 'Incorrect username and / or password.'})

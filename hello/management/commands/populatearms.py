c6118e3603b5f78e4757713790230904c83d53ac
Mustafa H <mhaid@cs.toronto.edu>
2020-12-27 19:09:08 -0500

    Fix bug populatearms not finding static dir

--- hello/management/commands/populatearms.py
+++ hello/management/commands/populatearms.py
@@ -1,4 +1,5 @@
 from django.core.management.base import BaseCommand, CommandError
+from django.conf import settings
 from hello.models import Arm
 import glob
 import os
@@ -12,8 +13,16 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.stdout.write(self.style.SUCCESS('Populating Database...'))
 
-        for name in glob.glob('C:/code/jamm-bandit/hello/static/images/*.bmp'):
+        filenames = glob.glob(os.path.join(settings.STATIC_ROOT, 'images/*.bmp'))
+        if not filenames:
+            self.stdout.write(self.style.SUCCESS('No images were found'))
+
+        for name in filenames:
             filename = os.path.basename(name)
+
+            # Django adds duplicates of the images that we have to ignore
+            if len(filename) > 16: 
+                continue
             # Check if the arm already exists to avoid duplicates
             arm = list(Arm.objects.filter(filename=filename))
             if not arm:
@@ -21,3 +30,5 @@ class Command(BaseCommand):
                 new_arm.save()
 
                 self.stdout.write(self.style.SUCCESS('Successfully created arm for "%s"' % filename))
+            else:
+                self.stdout.write(self.style.SUCCESS('Already exists: "%s"' % filename))

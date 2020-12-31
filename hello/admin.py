6db04d53f41a40c431fdec4cfaa22577057eee19
Emmy Liu <emmy.liu.1998@gmail.com>
2020-12-31 14:33:07 -0500

    register other models

--- hello/admin.py
+++ hello/admin.py
@@ -1,8 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
-from .models import Profile, Log, Arm
+from .models import Profile, Log, Arm, DuelRecord, Counter
 
 admin.site.register(Profile)
 admin.site.register(Log)
 admin.site.register(Arm)
+admin.site.register(DuelRecord)
+admin.site.register(Counter)

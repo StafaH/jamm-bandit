14faa684c78c57b698e579dec9ccfbf5c99ab469
Emmy Liu <emmy.liu.1998@gmail.com>
2021-01-03 11:00:50 -0500

    loop actually faster

--- hello/utils.py
+++ hello/utils.py
@@ -12,42 +12,50 @@ def dts_pick_first_arm(num_arms):
     timestep = Counter.objects.all()[0]
 
     estimated_samples = np.zeros((num_arms, num_arms))
-
     for i, j in combinations(range(num_arms), 2):
         duel_log = duel_history.filter(first_arm=i + 1, second_arm=j + 1).first()
         if duel_log is not None:
             wins = duel_log.first_arm_wins
             losses = duel_log.second_arm_wins
-            
-            
+
+            reverse_order = False
         else:
             duel_log = duel_history.filter(first_arm=j + 1, second_arm=i + 1).first()
             wins = duel_log.second_arm_wins
             losses = duel_log.first_arm_wins
             
+            reverse_order = True
+
         if wins + losses == 0:
             history = 1
             cb = 1
         else:
             history = wins / (wins + losses)
-            cb = sqrt((settings.ALPHA * log(timestep)) / (wins + losses)) 
+            cb = sqrt((settings.ALPHA * log(timestep)) / (wins + losses))
 
         upper_conf_bound[i][j] = history + cb
         lower_conf_bound[i][j] = history - cb
-    
+
         alpha = wins + 1
         beta = losses + 1
 
         estimated_samples[i][j] = np.random.beta(alpha, beta)
         estimated_samples[j][i] = 1 - estimated_samples[i][j]
 
+
     copeland_ub = (1 / (num_arms - 1)) * np.sum(
             upper_conf_bound, axis=1
     )
-    candidates =  [c[0] for c in np.argwhere(copeland_ub == np.amax(copeland_ub))]
+    candidates = np.argwhere(copeland_ub == np.amax(copeland_ub))
 
-    likely_wins = np.ma.masked_where(estimated_samples > 1/2, estimated_samples).mask.astype(int)
-    likely_wins[candidates] = 0
+    likely_wins = np.zeros((num_arms, num_arms))
+    for c in candidates:
+        i = c[0]
+        for j in range(num_arms):
+            if i == j:
+                continue
+            if estimated_samples[i][j] > 1 / 2:
+                likely_wins[i][j] = 1
 
     action = np.random.choice(
         np.argwhere(likely_wins == np.amax(likely_wins))[0]
